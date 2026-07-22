fd3f05a88cfe76771927dbaf2921c1fec27ac687
M KISHORE <kishoremaster41@gmail.com>
2026-07-22 12:06:12 +0530

    Updated demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int num = 7;
-    cout << num;
-
+ 
+int main(){
+    string s1="stravier";
+    for(int i=1;i<=10;i=i+1){
+        cout << "striver" <<endl;
+    }
     return 0;
 }
+    
