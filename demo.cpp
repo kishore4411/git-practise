607dd0170a13f2908e8edb7ff77ab06a139100c5
M KISHORE <kishoremaster41@gmail.com>
2026-07-23 12:14:10 +0530

    updated demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,15 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-void printname(string name){
-    cout << "hey " << name;
+
+int maxx(int num1,int num2){
+    if (num1>=num2){
+        return num1;
+    }
+    return num2;
 }
+
 int main(){
-    int num_1;
-    int num_2;
-    cin >> num_1 >> num_2;
-    
-    int num_3=num_1+num_2;
-    cout << "the value of num_3="<<num_3;
+    int num1,num2;
+    cin >> num1 >> num2;
+    int max=maxx(num1,num2);
+    cout << max;
 
     return 0;
 }
+    
