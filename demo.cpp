e1d98aac55df51cd238ebdc0c18ab619daf6b3b2
M KISHORE <kishoremaster41@gmail.com>
2026-07-25 11:27:13 +0530

    updated demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -36,12 +36,21 @@ void print4(int n){
         cout << endl;
     }
 }
+void print5(int n){
+    for (int i=1; i<n;i++){
+        for (int j=0;j<n-i+1;j++){
+            cout << "* ";
+        
+        }
+        cout << endl;
+    }
+}
 int main(){
     int t;
     cin >> t;
     for (int i=0;i<t;i++){
         int n;
         cin >> n;
-        print4(n);
+        print5(n);
     }
 }
