d30846057dbed93546f3903fefb2b6a983d1cb4b
M KISHORE <kishoremaster41@gmail.com>
2026-07-27 10:16:08 +0530

    updated patterns.cpp

---
+++ patterns.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print1(int n){
+    for (int i=0; i<n;i++){
+        for (int j=0;j<n;j++){
+            cout << "*";
+        
+        }
+        cout << endl;
+    }
+}
+void print2(int n){
+    for (int i=0; i<n;i++){
+        for (int j=0;j<=i;j++){
+            cout << "* ";
+        
+        }
+        cout << endl;
+    }
+}
+void print3(int n){
+    for (int i=1; i<=n;i++){
+        for (int j=1;j<=i;j++){
+            cout << j << " ";
+        
+        }
+        cout << endl;
+    }
+}
+void print4(int n){
+    for (int i=0; i<=n;i++){
+        for (int j=0;j<=i;j++){
+            cout << i << " ";
+        
+        }
+        cout << endl;
+    }
+}
+void print5(int n){
+    for (int i=1; i<n;i++){
+        for (int j=1;j<n-i+1;j++){
+            cout << j << " ";
+        
+        }
+        cout << endl;
+    }
+}
+void print6(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout << "*";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+
+        cout << endl;
+            
+    }
+}
+void print7(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+        for(int j=0;j<2*n-(2*i-1);j++){
+            cout << "*";
+        }
+        for(int j=0;j<i;j++){
+            cout << " ";
+        }
+
+        cout << endl;
+            
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for (int i=0;i<t;i++){
+        int n;
+        cin >> n;
+        print6(n);
+        print7(n);
+    }
+}
