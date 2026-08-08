8ef3e53c85c503ede341b549df5da489c8683d09
M KISHORE <kishoremaster41@gmail.com>
2026-08-08 10:50:40 +0530

    updated demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(){
+    cout << "raj";
+}
+int sum(int a, int b){
+    return a+b;
+}
+int main(){
+    print();
+    int s=sum(1,6);
+    cout << s;
+    return 0;
+}
